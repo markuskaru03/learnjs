650154988e8d34c59a476f87be05fd6b5e4f80ea
Markus Karu <markus.max.karu@gmail.com>
2025-10-07 09:31:46 +0300

    add boolean math examples

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-let text = 'Hello ' + 'World' + '!';
-console.log(text);
-text += 'yes?';
-console.log(text);
-text = "don't do this";
-text = 'don\'t do\n this\t';
-console.log(text);
-text = 'asdsadsadasdsadsadsadsadsadsadsadsadsa' +
-        'asdsadasdsadsadsadsadsadsadsadsadsad' +
-        'asdsdasdsadsadsadsadsadsadsadsadsaddsd'+
-        'asdsadsadsadsadsadsadsadasddsadsadsadsa';
-let name = 'Markus';
-let age = 22;
-text = `hello
-this is multi line
-text` ;
-console.log(text)
-text = 'Hello ' + name + '! You are ' + age +' years old!';
-text = `Hello ${name}! You are ${age} years old!`;
-console.log(text);
-console.log(text.length);
-console.log(text.toUpperCase());
-console.log(text.toLowerCase());
+let bool = true;
+bool = false;
+bool = true && false; // AND
+bool = false && true; // AND
+bool = false && false; // AND
+bool = true || false; // OR
+bool = false || true; // OR
+bool = false || false; // OR
+bool = true || true; // OR
+bool = !true; // NOT
+bool = !false; 
+bool = (true || false && true) && !(false || false || true && true);
 
-
-
+console.log(bool);
+bool = 10 > 5;
+console.log(bool);
+bool = 10 < 5;
+console.log(bool);
+bool = 10 > 10;
+console.log(bool);
+bool = 10 < 10;
+console.log(bool);
+bool = 10 == 10;
+console.log(bool);
+bool = 10 <= 10;
+bool = 10 >= 10;
+console.log(bool);
+bool = 10 != 4;
+console.log(bool);
+bool = '10' == 10;
+console.log(bool);
+bool = '10' === 10;
+console.log(bool);
+bool = '10' !== 10;
+console.log(bool);
