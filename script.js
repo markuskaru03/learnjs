d5147c86ffc3284431ecf9442a0f1fa17a213c52
Markus Karu <markus.max.karu@gmail.com>
2025-10-28 08:34:10 +0200

    Add sort and map examples

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let nums = [1,2,3,4,5,6,7,8,9];
+let nums = [6,3,5,7,9,4,2,8,1];
 
 nums.forEach(num => console.log(num));
 nums.forEach((num,i) => console.log(num,i));
@@ -7,3 +7,21 @@ let even = nums.filter(num => num%2 === 0);
 console.log(even);
 let sum = nums.reduce((sum, num) => sum+num);
 console.log(sum);
+
+let sorted = nums.sort((a,b) => {
+    if(a > b) {
+        return -1;
+    }
+    if (a < b){
+        return 1;
+    }
+    return 0;
+
+});
+console.log(sorted);
+let sorted2 = nums.sort((a,b) => b-a);
+console.log(sorted2);
+
+let squares = nums.map(num => num*num);
+
+console.log(squares);
