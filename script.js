f225c3313d3612f6fb6916a1ffd59edfb8c785b8
Markus Karu <markus.max.karu@gmail.com>
2025-11-04 09:04:49 +0200

    Add commonjs vs esmodulejs examples

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-console.log(window, {document});
-let heading = document.querySelector('h1');
-console.log({heading});
-heading.innerText = 'Hello DOM!';
+console.log('Hello terminal!');
 
-let button = document.querySelector('button');
+//const common = require('./common.js');
+import common from './common.js';
 
-function getRandomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+common.hello();
 
-button.addEventListener('click', event => {
-    console.log(event);
-    let color = getRandomColor();
-    heading.innerText =color;
-    heading.style.color = color;
-});
-setInterval(() => {
-document.documentElement.style.background = getRandomColor();
+import es from './module.js';
 
-}, 1000);
-
-let input = document.querySelector('input');
-
-input.addEventListener('input', event => {
-    console.log(event);
-    heading.innerText = input.value.split('').reverse().join('');
-});
-
+es.hello();
